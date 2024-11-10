6de53b827bfd968809444c2b541625fdd835b063
codehubnexus <codehubnexus@gmail.com>
2024-11-10 08:38:27 +0530

    Execution context

---
+++ HTML&CSS/index.js
@@ -0,0 +1,33 @@
+// console.log(a);
+
+// var a = 10;
+// console.log(a);
+
+// console.log(b, c);
+// let b = 20;
+
+// const c = 30;
+
+// console.log(b, c);
+a(30);
+function a(s) {
+  let aa = s;
+  console.log(a);
+}
+
+a(20);
+// b();
+
+let b = function () {
+  let a = 10;
+  console.log(a);
+};
+
+b();
+
+var c = () => {
+  let a = 2000;
+  console.log(a);
+};
+
+c();
